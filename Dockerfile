d9cf24287ff7312fc79a5044f2dd735aa2c3cdb7
thelastinuit <email@luisignac.io>
2020-03-22 15:36:21 +0100

    update website.

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM ruby:2.6-rc-alpine
-RUN apk --update add alpine-sdk nodejs tzdata
-RUN gem install middleman
-WORKDIR /code
-COPY . /code
-RUN bundle
--- push.sh
+++ push.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-bundle exec middleman build
-cp build/*.* .
-cp build/resume/* resume/
 git add .
 git commit -m "update website".
 git push -f
